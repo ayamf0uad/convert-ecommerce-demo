7547b465b22cbb7ae9eecf1bbcd6a5b160dc10f2
ayamf0uad <ayah.mohamedf@gmail.com>
2026-01-26 20:42:44 +0200

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
+import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
+import TrafficSimulator from './pages/TrafficSimulator';
 import DebugPanel from './components/DebugPanel';
 import CartDrawer from './components/CartDrawer';
 import { useConvert } from './context/ConvertContext';
@@ -28,7 +30,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/simulator" element={<TrafficSimulator />} />
         </Routes>
       </main>
       <Footer />
